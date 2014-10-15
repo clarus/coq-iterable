08b6dc0ade1fc9b99640fc06de97e4e578942ec9
Guillaume Claret <dev@clarus.me>
2014-10-15 16:44:08 +0200

    No monad used anymore

--- All.v
+++ All.v
@@ -1,49 +1,49 @@
 Require Import Coq.Lists.List.
 Require Import Coq.NArith.NArith.
+Require Import Coq.Strings.String.
 Require Import FunctionNinjas.All.
-Require Import Monad.All.
 
 Import ListNotations.
 Local Open Scope N.
 
-Module StateMonad.
-  Definition t (S : Type) (A : Type) : Type := S -> A * S.
-
-  Instance i (S : Type) : Monad.C (t S) := {
-    ret := fun _ x s => (x, s);
-    bind := fun _ _ x f s =>
-      let (x, s) := x s in
-      f x s }.
-
-  Definition get {S : Type} (_ : unit) : t S S :=
-    fun s => (s, s).
-
-  Definition set {S : Type} (s : S) : t S unit :=
-    fun _ => (tt, s).
-
-  Definition apply {S : Type} (f : S -> S) : t S unit :=
-    fun s => (tt, f s).
-
-  Definition run {S A : Type} (s : S) (x : t S A) : A * S :=
-    x s.
-End StateMonad.
-
 Module Iterable.
-  Class C (T : Type -> Type) : Type := {
-    iter : forall (M : Type -> Type) `{Monad.C M} {A : Type},
-      T A -> (A -> M unit) -> M unit }.
+  Class C (T A : Type) : Type := {
+    iter : forall {S E : Type},
+      S -> (S -> A -> S + E) -> T -> S + E }.
 
   Module Instance.
-    Instance list : C list := {
-      iter := fun M _ A =>
-        fix iter (l : list A) (f : A -> M unit) : M unit :=
+    Instance list (A : Type) : C (list A) A := {
+      iter := fun S E =>
+        fix iter (s : S) (f : S -> A -> S + E) (l : list A) : S + E :=
           match l with
-          | [] => Monad.ret tt
-          | x :: l => Monad.bind (f x) (fun _ => iter l f)
+          | [] => inl s
+          | x :: l =>
+            match f s x with
+            | inl s => iter s f l
+            | inr e => inr e
+            end
+          end }.
+
+    Instance string : C string Ascii.ascii := {
+      iter := fun S E =>
+        fix iter (s : S) (f : S -> Ascii.ascii -> S + E) (l : string) : S + E :=
+          match l with
+          | EmptyString => inl s
+          | String x l =>
+            match f s x with
+            | inl s => iter s f l
+            | inr e => inr e
+            end
           end }.
   End Instance.
 
-  Definition length {T : Type -> Type} `{C T} {A : Type} (l : T A) : N :=
-    snd (StateMonad.run 0 @@ iter (StateMonad.t N) l (fun _ =>
-      StateMonad.apply N.succ)).
+  Definition fold {T A : Type} `{C T A} {S : Type} (s : S)
+    (f : S -> A -> S) (l : T) : S :=
+    match iter (E := Empty_set) s (fun s x => inl @@ f s x) l with
+    | inl x => x
+    | inr e => match e with end
+    end.
+
+  Definition length {T A : Type} `{C T A} (l : T) : N :=
+    fold 0 (fun n _ => N.succ n) l.
 End Iterable.
